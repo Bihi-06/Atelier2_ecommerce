61e703ab0ec2f21f43dffabafd46c380074829c8
Bihi-06 <aitsaid.ayoub@etu.uae.ac.ma>
2024-12-04 13:41:20 +0100

    commit 4

--- store/src/main/java/com/fstt/store/beans/ClientBean.java
+++ store/src/main/java/com/fstt/store/beans/ClientBean.java
@@ -3,7 +3,10 @@ package com.fstt.store.beans;
 
 import com.fstt.store.persistence.Client;
 import com.fstt.store.utils.JpaUtil;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import lombok.Data;
@@ -11,19 +14,39 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Named
-@SessionScoped
+@RequestScoped
 @Data
 public class ClientBean implements Serializable {
+    private String nom;
+    private String email;
+    private String motDePasse;
+
     private EntityManager em;
-    private Client clientActuel;
+
 
     public ClientBean() {
         em = JpaUtil.getInstance().getEntityManager();
     }
 
-    public boolean estConnecte() {
-        return clientActuel != null;
+    public void inscrire() {
+        try {
+            Client client = new Client();
+            client.setNom(nom);
+            client.setEmail(email);
+            client.setMotDePasse(motDePasse);
+
+            em.getTransaction().begin();
+            em.persist(client);
+            em.getTransaction().commit();
+            FacesContext.getCurrentInstance().getApplication().getNavigationHandler().handleNavigation(FacesContext.getCurrentInstance(),null,"listArticles.xhtml");
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,"Erreur","Une erreur est survenue lors de l'inscription."));
+            e.printStackTrace();
+        }
     }
+
 
 
 }
